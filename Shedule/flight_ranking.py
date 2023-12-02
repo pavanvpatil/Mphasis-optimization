a5f060f8e853adcabe2d1b8d20099d7e5fa981b4
SamarthaT <136691411+SamarthaT@users.noreply.github.com>
2023-12-02 18:18:22 +0530

    Update flight_ranking.py

--- Shedule/flight_ranking.py
+++ Shedule/flight_ranking.py
@@ -1,22 +1,29 @@
-
+from datetime import datetime
 
 def isAltFlightPossible(id1, *id2)->bool:
-    if len(id2) > 3: #more than 3 connecting flights are not allowed
+    if len(id2) > 3: # more than 3 connecting flights are not allowed
         return False
-    max_dep_diff = 72 #in hours
+    max_dep_diff = 72 # in hours
     max_conn_diff = 12
-    dep1 = id1.departure_date.split('/') + id1.departure_time.split(':') #list of format ['mm','dd','yyyy','hh','mm']
-    dep2 = id2[0].departure_date.split('/') + id2.departure_time.split(':')
-    dep_diff = 24*(int(dep2[1])-int(dep1[1])) + (int(dep2[3])-int(dep1[3])) + (int(dep2[4])-int(dep1[4]))/60
+    dep1 = id1.departure_date +" "+ id1.departure_time # format mm/dd/yyyy hh:mm
+    dep2 = id2[0].departure_date +" "+ id2[0].departure_time
+    # converting the string input to date
+    start_date = datetime.strptime(dep1, "%m/%d/%Y %H:%M")
+    end_date = datetime.strptime(dep2, "%m/%d/%Y %H:%M")
+ 
+    # calculating the difference in dates
+    dep_diff = (end_date-start_date).total_seconds()/3600
     for x in range(len(id2)-1):
         '''
         We calculate difference in arrival of a connecting flight and
-        departure of the consecutive connecting flight and store it in 
+        departure of the consecutive connecting flight for all connecting flights and store it in 
         conn_diff
         '''
-        arr2 = id2[x].arrival_date.split('/') + id2.arrival_time.split(':')
-        dep2 = id2[x+1].departure_date.split('/') + id2.departure_time.split(':')
-        conn_diff = 24*(int(dep2[1])-int(arr2[1])) + (int(dep2[3])-int(arr2[3])) + (int(dep2[4])-int(arr2[4]))/60
+        arr2 = id2[x].arrival_date + " " + id2[x].arrival_time
+        dep2 = id2[x+1].departure_date + " " + id2[x+1].departure_time
+        start_date = datetime.strptime(arr2, "%m/%d/%Y %H:%M")
+        end_date = datetime.strptime(dep2, "%m/%d/%Y %H:%M")
+        conn_diff = (end_date-start_date).total_seconds()/3600
         if conn_diff<1 or conn_diff>max_conn_diff:
             return False
 
