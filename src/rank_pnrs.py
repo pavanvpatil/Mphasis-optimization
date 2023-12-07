6a50b1440bc121af093683006473b5490577ed4f
jp-karthik <karthikjagadeeshponarkar@gmail.com>
2023-12-07 16:16:53 +0530

    Variable renaming + optimized

--- src/rank_pnrs.py
+++ src/rank_pnrs.py
@@ -5,29 +5,51 @@ from src import passenger_dict
 from src.fetch_input.pnr_input import get_pnr_passenger_input
 
 
-def get_affected_pnrs(inventory_id: str) -> list[str]:
+def get_affected_pnrs(inventory_id: str) -> set[str]:
     '''This method returns a list of affected pnrs for a given inventory id
 
     :param inventory_id: Inventory ID of the affected flight
     :type inventory_id: str
-    :return: list of affected pnrs
-    :rtype: list[str] 
+    :return: set of affected pnrs
+    :rtype: set[str] 
     '''
 
-    affected_pnrs = []
+    affected_pnrs = set()
     schedule_id = inventory_dict[inventory_id].scheduleid
-    fno = schedule_dict[schedule_id].flightnumber
+    flight_number = schedule_dict[schedule_id].flightnumber
     carrier_code = schedule_dict[schedule_id].carriercode
     dep_date = inventory_dict[inventory_id].departuredate
 
-
-    for curr_booking in booking_dict:
-        if booking_dict[curr_booking].carrier_cd == carrier_code and booking_dict[curr_booking].flt_num == fno and booking_dict[curr_booking].dep_dt == dep_date:
-            affected_pnrs.append(curr_booking)
+    for booking_id in booking_dict:
+        if booking_dict[booking_id].carrier_cd == carrier_code and booking_dict[booking_id].flt_num == flight_number and booking_dict[booking_id].dep_dt == dep_date:
+            affected_pnrs.add(booking_dict[booking_id].recloc)
 
     return affected_pnrs
 
-def get_affected_passengers(inventory_id) -> list:
+
+def get_affected_bookings(inventory_id: str) -> set[str]:
+    '''This method returns a list of affected bookings for a given inventory id
+
+    :param inventory_id: Inventory ID of the affected flight
+    :type inventory_id: str
+    :return: set of affected bookings
+    :rtype: set[str] 
+    '''
+
+    affected_bookings = set()
+    schedule_id = inventory_dict[inventory_id].scheduleid
+    flight_no = schedule_dict[schedule_id].flightnumber
+    carrier_code = schedule_dict[schedule_id].carriercode
+    dep_date = inventory_dict[inventory_id].departuredate
+
+    for booking_id in booking_dict:
+        if booking_dict[booking_id].carrier_cd == carrier_code and booking_dict[booking_id].flt_num == flight_no and booking_dict[booking_id].dep_dt == dep_date:
+            affected_bookings.add(booking_id)
+
+    return affected_bookings
+
+
+def get_affected_passengers(inventory_id) -> list[str]:
     '''
     This function returns a list of doc_id of passengers
     param inventory_id : inventory id of affected flight
@@ -35,24 +57,17 @@ def get_affected_passengers(inventory_id) -> list:
     return list of doc_id of affected passengers
     rtype list[str]
     '''
-    af_pnrs_list = get_affected_pnrs(inventory_id)
-    af_pnrs = set()
-    for pnr in af_pnrs_list:
-        af_pnrs.add(booking_dict[pnr].recloc)
-    print(af_pnrs)
-    af_passengers = []
-    # for pnr in af_pnrs:
-    #     for passenger in passenger_dict:
-    #         if booking_dict[pnr].recloc == passenger_dict[passenger].recloc:
-    #             af_passengers.append(passenger_dict[passenger].doc_id)
-    for passenger in passenger_dict:
-        if passenger_dict[passenger].recloc in af_pnrs:
-            af_passengers.append(passenger_dict[passenger].doc_id)
+    affected_pnrs = get_affected_pnrs(inventory_id)
+    affected_passengers_doc_ids = []
+
+    for passenger_doc_id in passenger_dict:
+        if passenger_dict[passenger_doc_id].recloc in affected_pnrs:
+            affected_passengers_doc_ids.append(passenger_doc_id)
+
+    return affected_passengers_doc_ids
 
 
-    return af_passengers
-
-def rank_pnrs(inventory_id) -> list:
+def get_ranked_affected_passenger_doc_ids(inventory_id) -> list[str]:
     '''
     This method ranks the affected pnrs as per the pnr_ranking sheet
 
@@ -61,34 +76,40 @@ def rank_pnrs(inventory_id) -> list:
     return list of doc_id of passengers ranked as per pnr_score (highest score will be the first element)
     rtype : list[str]
     '''
+
     cabinJ = ["A", "D", "J"]
     cabinF = ["F", "B"]
     cabinY = ["S", "V", "W", "Z", "O", "S", "T", "U", "M", "N", "Y", "E", "L"]
-    affected_pnrs_list = get_affected_pnrs(inventory_id)
-    affected_pnrs = {}
-    recloc_score_dict = {}
-    for pnr in affected_pnrs_list:
-        affected_pnrs[pnr] = booking_dict[pnr]
-    for booking in affected_pnrs:
-        curr_booking = booking_dict[booking]
-        score = 0
-        score = score + int(curr_booking.pax_cnt) * 50
+
+    pnr_score_dict = {}
+    affected_booking_ids = get_affected_bookings(inventory_id)
+
+    for booking_id in affected_booking_ids:
+        curr_booking = booking_dict[booking_id]
+        pnr_score = 0
+        pnr_score = pnr_score + int(curr_booking.pax_cnt) * 50
         if curr_booking.cos_cd[0] in cabinF:
-            score = score + 1700
+            pnr_score = pnr_score + 1700
         elif curr_booking.cos_cd[0] in cabinJ:
-            score = score + 2000
+            pnr_score = pnr_score + 2000
         elif curr_booking.cos_cd[0] in cabinY:
-            score = score + 1500
-        recloc = affected_pnrs[booking].recloc
-        score = score + 750  # for class
-        recloc_score_dict[recloc] = score
-    aff_passengers_dict = {}
-    for passenger in passenger_dict:
-        if passenger_dict[passenger].recloc in recloc_score_dict:
-            score = recloc_score_dict[passenger_dict[passenger].recloc]
+            pnr_score = pnr_score + 1500
+        pnr = curr_booking.recloc
+        pnr_score = pnr_score + 750  # for class (should be checked further)
+        pnr_score_dict[pnr] = pnr_score
+
+    affected_passengers_dict = {}
+    for passenger_doc_id in passenger_dict:
+        passenger = passenger_dict[passenger_doc_id]
+        if passenger.recloc in pnr_score_dict:
+            pnr_score = pnr_score_dict[passenger.recloc]
             ssr_score = 0
-            if len(str(passenger_dict[passenger].ssr_code_cd1)) > 3:
-                ssr_score = 200 * (str(passenger_dict[passenger].ssr_code_cd1).count(",") + 1)
-            aff_passengers_dict[passenger_dict[passenger].doc_id] = ssr_score + score
-    op = sorted(aff_passengers_dict.keys(), key=lambda x: aff_passengers_dict[x], reverse=True)
-    return op
+            if len(str(passenger.ssr_code_cd1)) > 3:
+                ssr_score = 200 * \
+                    (str(passenger.ssr_code_cd1).count(
+                        ",") + 1)
+            affected_passengers_dict[passenger_doc_id] = ssr_score + pnr_score
+
+    ranked_affected_passenger_doc_ids = sorted(affected_passengers_dict.keys(),
+                                               key=lambda x: affected_passengers_dict[x], reverse=True)
+    return ranked_affected_passenger_doc_ids
